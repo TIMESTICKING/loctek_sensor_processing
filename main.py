1bf8c5de9e2ea2d81b494f3106bf38bb09c0b8fc
Jiabao Li <56912131+TIMESTICKING@users.noreply.github.com>
2024-03-07 16:29:04 +0800

    finish 0 to 40 degree

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import struct
 from drivers.Serial import *
-
+import numpy as np
+import cv2
+from scipy.ndimage import zoom
 
 serial = MySerial_2head1tail(b'\x88', 'COM3', b'\x77', b'\x66', 64 * 4)
 
@@ -15,3 +17,22 @@ def convert2float():
             IR_float.append(float_number)
 
         yield IR_float
+
+
+def play_IR():
+    for IR_float in convert2float():
+        IR_img = np.array(IR_float).reshape(8, 8)
+        # print(IR_img)
+        # IR_img = IR_img / IR_img.max()
+
+        zoomed_IR = zoom(IR_img, (20, 20), order=3).clip(0, 40)
+        zoomed_IR_int = np.asarray(zoomed_IR * 6.375, np.uint8)
+        heatmap = cv2.applyColorMap(zoomed_IR_int, cv2.COLORMAP_JET)
+
+        cv2.imshow('IR_img', heatmap)
+
+        key = cv2.waitKey(1)
+        if key == ord("q"):
+            break
+
+play_IR()
