e0c48764ca9ba346bbf997373b365eab41fc0bae
Jiabao Li <56912131+TIMESTICKING@users.noreply.github.com>
2024-03-15 11:17:12 +0800

    finish resave and bugs fixing

--- dataCollect.py
+++ dataCollect.py
@@ -4,6 +4,7 @@ import pathlib as pl
 import traceback
 import cv2
 import os
+import shutil
 
 SCENETYPE = ['a', 'b']
 
@@ -13,6 +14,8 @@ class DataCollect:
         self.heat_imgs = []
         self.recording = False
 
+        self.root = pl.Path('./data')
+
     def key_handler(self, key):
         if key == 32:
             # space
@@ -21,25 +24,46 @@ class DataCollect:
 
             if not self.recording and len(self.IR_imgs) > 0:
                 self.save_data()
+        elif key == 27:
+            # esc, to re-save the last round file to another directory
+            self.resave_data()
+
+    def resave_data(self):
+        new_scenetype = self._scenetype
+        new_sceneroot = self.root / pl.Path(new_scenetype)
+        os.makedirs(new_sceneroot, exist_ok=True)
+        try:
+            for ext in ['.mat', '.mp4']:
+                shutil.move(f'{self.last_sceneroot / self.last_filename}{ext}', 
+                            f'{new_sceneroot / self.last_filename}{ext}')
+            print("moving file completed!")
+
+            # save some parameters to instance
+            self.last_sceneroot = new_sceneroot
+            self.last_scenetype = new_scenetype
+        
+            # another change for re-saving the files
+            print("上一轮的存储是否想改变主意？按下ESC以重新保存，否则请忽略。")
+
+        except Exception as e:
+            traceback.print_exc()
 
 
     def save_data(self):
         print("Saving data...")
-        root = pl.Path('./data')
+        
 
-        print(list(zip(range(1, len(SCENETYPE)+1), SCENETYPE)))
         while True:
             try:
-                labels = int(input('Specify a scenetype from above ->'))
-                scenetype = SCENETYPE[labels-1]
-                sceneroot = root / pl.Path(scenetype)
+                scenetype = self._scenetype
+                sceneroot = self.root / pl.Path(scenetype)
                 timestamp = int(time.time())
                 filename = f'{scenetype}_{timestamp}'
                 os.makedirs(sceneroot, exist_ok=True)
 
                 # save IR_img np.array as mat
-                sio.savemat(sceneroot / filename,
-                            {'IR_video': np.array(self.IR_imgs)})
+                sio.savemat(f'{sceneroot / filename}.mat',
+                            {'IR_video': np.array(self.IR_imgs)}, appendmat=True)
                 
                 # save preview heatmap videos
                 fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Define the codec
@@ -47,13 +71,33 @@ class DataCollect:
                 for hmap in self.heat_imgs:
                     video_writer.write(hmap) 
                 video_writer.release()
+
+                # clear buffer
+                self.heat_imgs = []
+                self.IR_imgs = []
+
+                # save some parameters to instance
+                self.last_sceneroot = sceneroot
+                self.last_scenetype = scenetype
+                self.last_filename = filename
+
+                # another change for re-saving the files
+                print("上一轮的存储是否想改变主意？按下ESC以重新保存，否则请忽略。")
                 
                 break
             except Exception as e:
                 traceback.print_exc()
     
 
-            
+    @property
+    def _scenetype(self):
+        print(list(zip(range(1, len(SCENETYPE)+1), SCENETYPE)))
+        labels = int(input('Specify a scenetype from above ->'))
+        scenetype = SCENETYPE[labels-1]
+
+        return scenetype
+
+
 
     def pre_zoom(self, IR_img):
         if self.recording:
